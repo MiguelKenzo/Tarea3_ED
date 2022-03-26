1036c97d76f58bd51dfba09605dd560bc3453afc
MiguelKenzo <miguelangeldelatorre7@gmail.com>
2022-03-26 12:15:21 +0100

    First commit

---
+++ Tarea3_ED/src/Tarea3.java
@@ -0,0 +1,10 @@
+
+public class Tarea3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("En este proyecto trabajamos con el repositorio GITHUB");
+	}
+
+}
